235788086fbc97b19ded6f7c13fe798b354d0873
Oleg Moiseev <guest363@gmail.com>
2021-11-12 18:48:49 +0300

    Fix: stream works

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+import { pipeline } from "stream";
 import { transformerSelector } from "./cipher/transformers/transformer-selector.js";
 import { cliParser } from "./cli/cli-parser.js";
 import { ioReader } from "./io-reader/io-reader.js";
 
 const cliParams = cliParser(process);
 const commands = cliParams.command;
-console.log(cliParams);
 
 const readStream = ioReader({ param: cliParams.input, rwType: "read" });
 const writeStream = ioReader({ param: cliParams.output, rwType: "write" });
@@ -15,16 +15,24 @@ const writeStream = ioReader({ param: cliParams.output, rwType: "write" });
  * стакать потоков через метод .pipe()
  */
 let currentPipe = readStream;
+
 /**
  * Накидываем потоки шифрования
  */
 for await (const command of commands) {
   const cipherType = command[0];
   const cipherAction = command[1];
-  const cipherPipe = transformerSelector[cipherType](cipherAction);
+  const cipherTransformer = transformerSelector[cipherType](cipherAction);
 
-  currentPipe = currentPipe.pipe(cipherPipe);
+  currentPipe = await pipeline(currentPipe, cipherTransformer, (err) => {
+    if (err) {
+      console.error(`Failed ${cliParams.action}`, err);
+    } else {
+      console.log(`File success ${cliParams.action}`);
+    }
+  });
 }
+
 /**
  * В завершении пишем все это дело в writeStream
  */
