a2416d188c49fdcdd3f90ab2955baeda83865025
Oleg Moiseev <guest363@gmail.com>
2021-11-12 10:53:47 +0300

    Fix: test command

--- cli/test-commands.js
+++ cli/test-commands.js
@@ -1,23 +1,25 @@
 import { errorAction } from "../error-action.js";
-const cipherTypes = ["c", "r", "a"];
+import { isCurrectCipherType } from "./is-currect-cipher-type.js";
+import { isCurrectCipherParam } from "./is-current-cipher-param.js";
+
 /**
  * Тестирует последовательность команд на корректность
  * В случае ошибки завершает приложение
  */
 export const testCommands = (command) => {
-  const firstSymbol = command[0].toLowerCase();
-  const secondSymbol = command[1];
+  const cipherSymbol = command[0].toLowerCase();
+  const cipherParam = command[1];
 
-  if (cipherTypes.indexOf(firstSymbol) === -1) {
+  if (isCurrectCipherType()) {
     errorAction(
       "Invalid command options, set current cipher type. \n C - for Caesar, \n A - for Atbash, \n R - for ROT-8"
     );
   }
-  if (firstSymbol === "a") {
-    if (secondSymbol !== void 0) {
+  if (cipherSymbol === "a") {
+    if (cipherParam) {
       errorAction("Atbash cipher has no second param");
     }
-  } else if (secondSymbol !== "0" || secondSymbol !== "1") {
+  } else if (isCurrectCipherParam(cipherParam)) {
     errorAction("No encode/decode param");
   }
 };
