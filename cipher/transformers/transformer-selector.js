7b8f418393570bbcbbe98a19e3d5a87b700db147
Oleg Moiseev <guest363@gmail.com>
2021-11-13 11:16:55 +0300

    Clean

--- cipher/transformers/transformer-selector.js
+++
@@ -1,9 +0,0 @@
-import { atbashTransformer } from "./atbash-transformer.js";
-import { r8Transformer } from "./r8-tarnsformer.js";
-import { caesarTransformer } from "./сaesar-transformer.js";
-
-export const transformerSelector = {
-  r: r8Transformer,
-  c: caesarTransformer,
-  a: atbashTransformer,
-};
--- cli/is-current-cipher-param.js
+++
@@ -1,7 +0,0 @@
-const cipherParams = ["0", "1"];
-
-/**
- * Проверяет поддерживается ли данный шифр
- */
-export const isCurrectCipherParam = (cipherParam) =>
-  cipherParams.indexOf(cipherParam) === -1;
