89bb11bc87c3805c80a725ee22016becbf5b4711
Oleg Moiseev <guest363@gmail.com>
2021-11-19 20:17:34 +0300

    Add: cli template test

---
+++ cli/tests/cli-parser.spec.js
@@ -0,0 +1,9 @@
+import { describe, test } from "@jest/globals";
+/* import { CustomError } from "../../custom-error.js"; */
+import { cliParser } from "../parse-commnads.js";
+
+describe("Проверка работы функции cliParser. Мокаем process", () => {
+ /*  test("Сommand С1-С0 ✔️", async () => {
+    expect(cliParser("C1-C0")).toStrictEqual(["C1", "C0"]);
+  }); */
+});
